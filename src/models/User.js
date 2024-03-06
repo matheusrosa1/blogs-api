eae264883a5c538c4fe6b652b62616887f21ea77
Matheus Rosa <matheusrosataxa@gmail.com>
2024-03-06 18:02:54 -0300

    requisito 2 finalizado

---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true,
+  });
+
+/*   User.associate = (models) => {
+    User.hasToMany(models.)
+  }
+ */
+  return User;
+}
+
+module.exports = UserModel;
